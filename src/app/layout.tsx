9b0ec347614b7c9914c33fc95ba10bc0505dbb79
Jane Alam Tusher <jaanealamtusher@gmail.com>
2024-01-09 23:25:17 +0600

    region added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,8 @@ export const metadata: Metadata = {
     },
 };
 
+export const preferredRegion = ['sin1'];
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en" suppressHydrationWarning>
